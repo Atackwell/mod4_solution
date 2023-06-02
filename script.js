518d0a5eaa951a690bfb0bf1810e072f3d3be6f7
Atackwell <129234735+Atackwell@users.noreply.github.com>
2023-06-01 20:14:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+(function() {
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	for (var i =0; i < names.length; i++) {
+		var firstLetter = names[i].charAT(0).toLowerCase();
+		if (firstLetter === 'j'){
+			ByeSpeaker.speak(names[i]);
+		} else {
+			HelloSpeaker.speak(names[i]);
+		}
+	}
+})();
